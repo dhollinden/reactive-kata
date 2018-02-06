7dc4dc1c6bd903b22bc06c671fa877f2b43c18fc
dhollinden <dave.hollinden@ithaka.org>
2018-02-06 06:53:42 -0500

    pass all tests

--- react.js
+++ react.js
@@ -7,6 +7,11 @@ class InputCell {
 
   setValue(value) {
     this.value = value;
+    this.saveDependentCellValues();
+  }
+
+  saveDependentCellValues() {
+    this.dependentCells.forEach(function(item) { item.saveCellValue(); });
     this.updateDependentCells();
   }
 
@@ -36,20 +41,28 @@ class ComputeCell {
     this.inputArray = inputArray;
     this.fn = fn;
     this.value = fn(inputArray);
+
     this.dependentCells = [];
     this.callbackCells = [];
-    this.storeDependentCells();
-    this.priorValue;
+    this.savedValue = "";
+
+    this.storeDependencies();
   }
 
-  storeDependentCells() {
-    for (var i=0; i<this.inputArray.length; i++) {
-      this.inputArray[i].dependentCells.push(this);
-    }
+  storeDependencies() {
+    this.inputArray.forEach(function(item) { item.dependentCells.push(this); }, this)
+  }
+
+  saveCellValue() {
+    this.savedValue = this.value;
+    this.saveDependentCellValues();
+  }
+
+  saveDependentCellValues() {
+    this.dependentCells.forEach(function(item) { item.saveCellValue(); })
   }
 
   updateCell() {
-    this.priorValue = this.value;
     this.value = this.fn(this.inputArray);
     this.updateDependentCells();
   }
@@ -59,38 +72,29 @@ class ComputeCell {
   }
 
   updateCallbackCells() {
-    if (this.value != this.priorValue) {
-      for (var i=0; i<this.callbackCells.length; i++) {
-        let result = this.callbackCells[i].fn(this);
-        if (result != this.callbackCells[i].values[this.callbackCells[i].values.length - 1]) {
-          this.callbackCells[i].values.push(result);
+    if (this.value !== this.savedValue) { // check if ComputeCell value has changed
+      this.callbackCells.forEach(function(e) {
+        let newCallbackValue = e.fn(this);
+        let currentCallbackValue = e.values[e.values.length - 1];
+        if (newCallbackValue !== currentCallbackValue) { //check if CallbackCell needs to be updated
+          e.values.push(newCallbackValue);
         }
-      }
+      }, this)
     }
     this.updateDependentCallbackCells();
   }
 
   updateDependentCallbackCells() {
-    this.dependentCells.forEach(function(item) {
-      item.updateCallbackCells();
-    })
+    this.dependentCells.forEach(function(item) { item.updateCallbackCells(); })
   }
 
-  addCallback(callbackCell) {
-    this.callbackCells.push(callbackCell);
-  }
+  addCallback(callbackCell) { this.callbackCells.push(callbackCell); }
 
   removeCallback(callbackCell) {
     let index = this.callbackCells.indexOf(callbackCell);
-    if (index != -1)
+    if (index !== -1)
       this.callbackCells.splice(index, 1);
   }
-
-// *when I use forEach in updateCallbackCells, it fails with this = undefined
-//    this.callbackCells.forEach(function(item) {
-//      let result = item.fn(this);
-//      item.values.push(result);
-//    })
 
 }
 
